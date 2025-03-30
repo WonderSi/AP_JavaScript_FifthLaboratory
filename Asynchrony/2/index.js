bf26565c5842c7c98f3fa5eb4e6b0d380330792d
WonderSi <kirillkozlov00@gmail.com>
2025-03-30 13:49:56 +0700

    create task 2

---
+++ Asynchrony/2/index.js
@@ -0,0 +1,45 @@
+// Асинхронные функции
+function readConfig (name) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('(1) config from ' + name + ' loaded')
+            resolve();
+        }, Math.floor(Math.random() * 1000))
+    })
+}
+function doQuery (statement) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('(2) SQL query executed: ' + statement)
+            resolve();
+        }, Math.floor(Math.random() * 1000))
+    })
+}
+function httpGet (url) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('(3) Page retrieved: ' + url)
+            resolve();
+        }, Math.floor(Math.random() * 1000))
+    })
+}
+function readFile (path) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('(4) Readme file from ' + path + ' loaded')
+            resolve();
+        }, Math.floor(Math.random() * 1000))
+    })
+}
+// Вызов функций
+console.log('start')
+
+readConfig('myConfig')
+    .then(() => doQuery('select * from cities'))
+    .then(() => httpGet('http://google.com'))
+    .then(() => readFile('README.md'))
+    .then(() => {
+        console.log('It is done!')
+    })
+    .catch(error => console.error(error))
+console.log('end')
